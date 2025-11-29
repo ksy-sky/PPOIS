193abc4d1aca222bd6030f847b5c0da67af9a363
ksy-sky <ksenia.ganetskaya@gmail.com>
2025-11-29 09:02:13 +0300

    Update and rename GraphTestTypes.h to City.h

--- lab4/Graph/City.h
+++ lab4/Graph/City.h
@@ -8,28 +8,11 @@ public:
     int population;
     
     City(const std::string& n = "", int p = 0) : name(n), population(p) {}
-    
     bool operator==(const City& other) const {
         return name == other.name && population == other.population;
     }
-    
     friend std::ostream& operator<<(std::ostream& os, const City& c) {
         return os << c.name << "(" << c.population << ")";
     }
 };
 
-class Course {
-public:
-    std::string title;
-    int credits;
-    
-    Course(const std::string& t = "", int c = 0) : title(t), credits(c) {}
-    
-    bool operator==(const Course& other) const {
-        return title == other.title && credits == other.credits;
-    }
-    
-    friend std::ostream& operator<<(std::ostream& os, const Course& c) {
-        return os << c.title << " (" << c.credits << " credits)";
-    }
-};
