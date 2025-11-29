3a25bf3a009d8dd5c4007de2566324aa0be3fee1
ksy-sky <ksenia.ganetskaya@gmail.com>
2025-11-29 11:08:33 +0300

    Update BozoSort.h

--- lab4/Sort/BozoSort.h
+++ lab4/Sort/BozoSort.h
@@ -1,30 +1,39 @@
 #pragma once
 #include <algorithm>
 #include <random>
+#include <vector>
 
 constexpr std::size_t MAX_ITERATIONS = 10000;
-template<typename Container>
-void bozo_sort(Container& container) {
-    auto first = container.begin();
-    auto last = container.end();
-    
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    auto size = std::distance(first, last);
-    
-    if (size <= 1) return;
-    
-    std::uniform_int_distribution<> dis(0, size - 1);
-    
-    size_t iterations = 0;
-    
-    while (!std::is_sorted(first, last) && iterations < MAX_ITERATIONS) {
-        auto leftIndex = dis(gen);
-        auto rightIndex = dis(gen);
 
-        if (leftIndex != rightIndex) {
-            std::iter_swap(first + leftIndex, first + rightIndex);
-        }
-        iterations++;
+template<typename T>
+class BozoSorter {
+public:
+    using Container = std::vector<T>;
+    void sort(Container& container) {
+        if (container.size() <= 1) return;
+        bozo_sort(container.begin(), container.end());
     }
-}
+
+private:
+    template<typename Iterator>
+    void bozo_sort(Iterator first, Iterator last) {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+
+        auto size = std::distance(first, last);
+        if (size <= 1) return;
+
+        std::uniform_int_distribution<> dis(0, size - 1);
+        std::size_t iterations = 0;
+
+        while (!std::is_sorted(first, last) && iterations < MAX_ITERATIONS) {
+            auto leftIndex = dis(gen);
+            auto rightIndex = dis(gen);
+
+            if (leftIndex != rightIndex) {
+                std::iter_swap(first + leftIndex, first + rightIndex);
+            }
+            iterations++;
+        }
+    }
+};
